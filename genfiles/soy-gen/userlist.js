f5389ddab243f9396a4f8a7a9af55c59137c98f4
Eddy M <wilddamon@gmail.com>
2012-04-16 23:03:58 +1000

    Adds the link as a separate column in the table

--- genfiles/soy-gen/userlist.js
+++ genfiles/soy-gen/userlist.js
@@ -15,7 +15,7 @@ goog.require('soy.StringBuilder');
  */
 calendarmailer.soy.userlist.all = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
-  output.append('<table id="userlist-table" class="userlist"><tbody><tr><th>Name</th><th>Total events</th><th>Event name</th><th>Event location</th><th>Start time</th><th>Event recurrence</th><th>User action</th></tr>');
+  output.append('<table id="userlist-table" class="userlist"><tbody><tr><th>Name</th><th>Total events</th><th>Event name</th><th>Event link</th><th>Event location</th><th>Start time</th><th>Event recurrence</th><th>User action</th></tr>');
   var userList53 = opt_data.users;
   var userListLen53 = userList53.length;
   for (var userIndex53 = 0; userIndex53 < userListLen53; userIndex53++) {
@@ -40,12 +40,12 @@ calendarmailer.soy.userlist.row = function(opt_data, opt_sb) {
   var eventListLen60 = eventList60.length;
   for (var eventIndex60 = 0; eventIndex60 < eventListLen60; eventIndex60++) {
     var eventData60 = eventList60[eventIndex60];
-    output.append('<tr ', (opt_data.user.num_events == 1) ? '' : (opt_data.user.num_events == 2) ? 'class="userlist-two"' : (opt_data.user.num_events <= 4) ? 'class="userlist-four"' : (opt_data.user.num_events > 4) ? 'class="userlist-lots"' : '', '><td>', soy.$$escapeHtml(opt_data.user.name), '</td><td>', soy.$$escapeHtml(opt_data.user.num_events), '</td><td><a href="', soy.$$escapeHtml(eventData60.link), '">', soy.$$escapeHtml(eventData60.summary), '</a></td><td>', soy.$$escapeHtml(eventData60.location), '</td><td>', (eventData60.startTime) ? soy.$$escapeHtml(eventData60.startTime) : 'All day', '</td><td><ul>');
-    var recurStrList87 = eventData60.recurrence;
-    var recurStrListLen87 = recurStrList87.length;
-    for (var recurStrIndex87 = 0; recurStrIndex87 < recurStrListLen87; recurStrIndex87++) {
-      var recurStrData87 = recurStrList87[recurStrIndex87];
-      output.append('<li>', soy.$$escapeHtml(recurStrData87), '</li>');
+    output.append('<tr ', (opt_data.user.num_events == 1) ? '' : (opt_data.user.num_events == 2) ? 'class="userlist-two"' : (opt_data.user.num_events <= 4) ? 'class="userlist-four"' : (opt_data.user.num_events > 4) ? 'class="userlist-lots"' : '', '><td>', soy.$$escapeHtml(opt_data.user.name), '</td><td>', soy.$$escapeHtml(opt_data.user.num_events), '</td><td><a href="', soy.$$escapeHtml(eventData60.link), '">', soy.$$escapeHtml(eventData60.summary), '</a></td><td>', soy.$$escapeHtml(eventData60.link), '</td><td>', soy.$$escapeHtml(eventData60.location), '</td><td>', (eventData60.startTime) ? soy.$$escapeHtml(eventData60.startTime) : 'All day', '</td><td><ul>');
+    var recurStrList89 = eventData60.recurrence;
+    var recurStrListLen89 = recurStrList89.length;
+    for (var recurStrIndex89 = 0; recurStrIndex89 < recurStrListLen89; recurStrIndex89++) {
+      var recurStrData89 = recurStrList89[recurStrIndex89];
+      output.append('<li>', soy.$$escapeHtml(recurStrData89), '</li>');
     }
     output.append('</ul></td><td>', soy.$$escapeHtml(eventData60.state), '</td></tr>');
   }
@@ -78,11 +78,11 @@ calendarmailer.soy.userlist.calendarList = function(opt_data, opt_sb) {
   var output = opt_sb || new soy.StringBuilder();
   output.append('<ul id="userlist-calendarlist">');
   if (opt_data.calendars) {
-    var calendarList104 = calendars;
-    var calendarListLen104 = calendarList104.length;
-    for (var calendarIndex104 = 0; calendarIndex104 < calendarListLen104; calendarIndex104++) {
-      var calendarData104 = calendarList104[calendarIndex104];
-      calendarmailer.soy.userlist.calendarListRow({calendar: calendarData104}, output);
+    var calendarList106 = calendars;
+    var calendarListLen106 = calendarList106.length;
+    for (var calendarIndex106 = 0; calendarIndex106 < calendarListLen106; calendarIndex106++) {
+      var calendarData106 = calendarList106[calendarIndex106];
+      calendarmailer.soy.userlist.calendarListRow({calendar: calendarData106}, output);
     }
   }
   output.append('</ul>');
