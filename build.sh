7d31655edbccc9b5fc5dc651d52ff25a9f8414e8
wilddamon <wilddamon@gmail.com>
2013-10-30 17:35:51 +1100

    use symbols for closure and soy outputs

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ echo "***Running SoyToJsSrcCompiler***"
 java -jar ../soy-latest/SoyToJsSrcCompiler.jar \
   --shouldGenerateJsdoc \
   --shouldProvideRequireSoyNamespaces \
-  --outputPathFormat genfiles/soy-gen/{INPUT_FILE_NAME_NO_EXT}.js \
+  --outputPathFormat $soy_out_dir/{INPUT_FILE_NAME_NO_EXT}.js \
   ${soy_files[@]}
 
 echo "***Running closurebuilder***"
@@ -35,26 +35,26 @@ echo "***Running closurebuilder***"
     --root=../closure-library/ \
     --root=../soy-latest/ \
     --root=javascript/ \
-    --root=genfiles/soy-gen \
+    --root=$soy_out_dir \
     --namespace="calendarmailer.picker.App" \
     --output_mode=script \
     --compiler_jar=../compiler-latest/compiler.jar \
-    > genfiles/closure-gen/pickerapp.js
+    > $closure_out_dir/pickerapp.js
 ../closure-library/closure/bin/build/closurebuilder.py \
     --root=../closure-library/ \
     --root=../soy-latest/ \
     --root=javascript/ \
-    --root=genfiles/soy-gen \
+    --root=$soy_out_dir \
     --namespace="calendarmailer.dashboard.App" \
     --output_mode=script \
     --compiler_jar=../compiler-latest/compiler.jar \
-    > genfiles/closure-gen/dashboard.js
+    > $closure_out_dir/dashboard.js
 
 echo "***Running css compiler***"
 java -jar ../css-compiler/closure-stylesheets-20111230.jar \
     ../closure-library/closure/goog/css/checkbox.css \
     ../closure-library/closure/goog/css/common.css \
     ${css_files[@]} \
-    --output-file genfiles/closure-gen/app.css \
+    --output-file $closure_out_dir/app.css \
     --pretty-print
 
